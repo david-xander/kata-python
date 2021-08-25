52fd5c1e4f7d42716d9ed8b956937e505c55e63b
david-xander <ddelgado@a2sistemas.com>
2021-08-25 12:36:59 +0200

    end

--- ClosestToZeroR3/hiker.py
+++ ClosestToZeroR3/hiker.py
@@ -1,6 +1,66 @@
+import math
+
 class Hiker:
-    pass
+    
+    ints = []
+    
+    def __init__(self, input, *args, **kwargs):
+        super(Hiker, self).__init__(*args, **kwargs)
+        self.validate(input)
+        self.ints = self.merge_sort(input)
+
+    def closest(self):
+        res = math.inf
+        for item in self.ints:
+            if abs(item) <= abs(res):
+                res = item
+        return res
+
+    def merge_sort(self, input):
+        # divide and conquer part
+        n = len(input)
+        half = n//2
+
+        L = input[:half]
+        R = input[half:]
+
+        if len(L) > 1:
+            L = self.merge_sort(L)
+        if len(R) > 1:
+            R = self.merge_sort(R)
+
+        return self.merge(L, R)
+
+    def merge(self, L, R):
+        res = []
+        n = len(L) + len(R)
+        i = 0
+        j = 0
+        k = 0
+        while i < n:
+            if L[j] <= R[k]:
+                res.append(L[j])
+                if j < len(L)-1:
+                    j += 1
+                else:
+                    L[j] = math.inf
+            else:
+                res.append(R[k])                
+                if k < len(R)-1:
+                    k += 1
+                else:
+                    R[k] = math.inf
+            i += 1
+
+        return res
+
+
+    def validate(self, input):
+        if not isinstance(input, list): raise Exception
+        for item in input:
+            if not isinstance(item, int): raise Exception
+        
+    
 
 if __name__ == '__main__':
     pass
-    
--- ClosestToZeroR3/test_hiker.py
+++ ClosestToZeroR3/test_hiker.py
@@ -3,8 +3,29 @@ import unittest
 
 
 class TestHiker(unittest.TestCase):
-    def test_something(self):
-        pass
+    
+    def test_input_is_list(self):
+        with self.assertRaises(Exception):
+            h = Hiker('not list')
+    
+    def test_input_all_items_are_ints(self):
+        with self.assertRaises(Exception):
+            h = Hiker([1, 2, 'not int'])
+
+    def test_all_items_are_there(self):
+        input = [1, 2, 3, 4, 5]
+        h = Hiker(input)
+        self.assertEqual(input, h.ints)
+
+    def test_all_items_are_ordered(self):
+        input = [5, 2, 1, -1, 3, -2]
+        h = Hiker(input)
+        self.assertEqual([-2,-1,1,2,3,5], h.ints)
+
+    def test_all_closest_method(self):
+        input = [5, 2, 1, -1, 3, -2]
+        h = Hiker(input)
+        self.assertEqual(1, h.closest())
 
 if __name__ == '__main__':
     unittest.main()  # pragma: no cover
